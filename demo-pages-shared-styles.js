3b50d4795907d965e550640aa986e9d5ba86146b
Pawel Psztyc <jarrodek@gmail.com>
2019-07-30 12:39:20 -0700

    feat: moving styles from API page to default styles

--- demo-pages-shared-styles.js
+++ demo-pages-shared-styles.js
@@ -27,6 +27,85 @@ body {
   margin-left: auto;
   margin-right: auto;
 }
+
+.card {
+  box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14),
+              0 1px 10px 0 rgba(0, 0, 0, 0.12),
+              0 2px 4px -1px rgba(0, 0, 0, 0.4);
+  padding: 20px;
+  margin-top: 20px;
+  margin-bottom: 20px;
+  margin-left: 20px;
+  box-sizing: border-box;
+  background-color: #fff;
+}
+
+header {
+  padding: 12px 24px;
+  background-color: #C5E1A5;
+  display: flex;
+  align-items: center;
+  --iron-icon-fill-color: #3E2723;
+  --paper-input-container-focus-color: #33691E;
+  --paper-input-container-label: {
+    color: #3E2723;
+  };
+}
+
+.spacer {
+  flex: 1;
+}
+
+.settings-action-item {
+  height: 48px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 16px;
+  box-sizing: border-box;
+}
+
+body.styled {
+  margin: 0;
+  padding: 0;
+  background-color: #fff;
+  height: 100vh;
+  --primary-color: #00A2DF;
+  --arc-font-body1-font-size: 14px;
+  --arc-font-body1-font-weight: 400;
+  --arc-font-body1-line-height: 20px;
+  --arc-font-code-family: 'Roboto Mono', 'Consolas', 'Menlo', monospace;
+  --code-background-color: #f5f2f0;
+}
+
+body.styled.dark {
+  background-color: #424242;
+  height: 100vh;
+  color: #fff;
+  --primary-color: #2196f3;
+  --primary-text-color: #fff;
+  --paper-toggle-button-label-color: #fff;
+  --paper-listbox-background-color: #424242;
+  --paper-listbox-color: #fff;
+}
+
+body.styled.dark .card {
+  background-color: #424242;
+}
+
+body.styled.dark header {
+  background-color: #212121;
+  --iron-icon-fill-color: #fff;
+  --paper-input-container-color: rgba(255, 255, 255, 0.84);
+}
+
+body.styled.dark .settings-action-item {
+  background-color: #212121;
+}
+
+.demo-container {
+  flex: 1;
+}
 `;
 try {
   document.adoptedStyleSheets = document.adoptedStyleSheets.concat(style.styleSheet);
