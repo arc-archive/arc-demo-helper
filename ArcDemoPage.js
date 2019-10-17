24e914964dfc540b4586beac5026798f3e83108a
Pawel Psztyc <jarrodek@gmail.com>
2019-10-16 17:32:36 -0700

    chore: removing paper-menu button from demo page

--- ArcDemoPage.js
+++ ArcDemoPage.js
@@ -1,8 +1,4 @@
 import { html, render } from 'lit-html';
-import '@polymer/paper-menu-button/paper-menu-button.js';
-import '@polymer/iron-icons/iron-icons.js';
-import '@polymer/paper-icon-button/paper-icon-button.js';
-import '@polymer/paper-toggle-button/paper-toggle-button.js';
 import './demo-pages-shared-styles.js';
 /**
  * Base class for ARC components demo page.
@@ -117,51 +113,7 @@ export class ArcDemoPage {
    * ```
    */
   contentTemplate() {}
-  /**
-   * Returns the template for the header action controls.
-   *
-   * Override this to add different controls.
-   *
-   * @return {Object} HTML template
-   */
-  headerControlsTemplate() {
-    return html`<paper-menu-button dynamic-align>
-    <paper-icon-button
-      aria-label="Press to toggle demo page settings menu"
-      icon="settings"
-      slot="dropdown-trigger"></paper-icon-button>
-    <div slot="dropdown-content">
-      ${this._headerControlsTemplate()}
-    </div>`;
-  }
-  /**
-   * Returns list of options for header controld
-   *
-   * Override this to add different controls:
-   *
-   * ```javascript
-   * _headerControlsTemplate() {
-   *  return html`
-   *  ${super._headerControlsTemplate()}
-   *  <div class="settings-action-item">
-   *    <paper-toggle-button @checked-changed="${this._otherHandler}">Other option</paper-toggle-button>
-   *  </div>`;
-   * }
-   * ```
-   *
-   * @return {Object} HTML template
-   */
-  _headerControlsTemplate() {
-    return html`<div class="settings-action-item">
-      <paper-toggle-button @checked-changed="${this._darkThemeHandler}">Toggle dark theme</paper-toggle-button>
-    </div>
-    <div class="settings-action-item">
-      <paper-toggle-button @checked-changed="${this._narrowHandler}">Toggle narrow attribute</paper-toggle-button>
-    </div>
-    <div class="settings-action-item">
-      <paper-toggle-button checked @checked-changed="${this._stylesHandler}">Toggle styles</paper-toggle-button>
-    </div>`;
-  }
+
   /**
    * Call this on the top of the `render()` method to render demo navigation
    * @return {Object} HTML template for demo header
@@ -171,8 +123,6 @@ export class ArcDemoPage {
     return html`
     <header>
       <h1>${componentName}</h1>
-      <div class="spacer"></div>
-      ${this.renderViewControls ? this.headerControlsTemplate() : undefined}
     </header>`;
   }
 
