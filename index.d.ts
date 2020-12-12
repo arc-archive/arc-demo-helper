5f08c9d6b6a45e91718e39648749534261d1af56
Pawel Psztyc <jarrodek@gmail.com>
2020-12-12 12:02:59 -0800

    fix: adding missing exports
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,4 @@
 export {DemoPage} from './src/DemoPage';
 export {ApiDemoPage} from './src/ApiDemoPage';
+export * as EncodingHelpers from './src/EncodingHelpers';
+export { ExportHandlerMixin } from './src/ExportHandlerMixin';
--- index.js
+++ index.js
@@ -1,2 +1,4 @@
 export { DemoPage } from './src/DemoPage.js';
 export { ApiDemoPage } from './src/ApiDemoPage.js';
+export * as EncodingHelpers from './src/EncodingHelpers.js';
+export { ExportHandlerMixin } from './src/ExportHandlerMixin.js';
