b5f3f0efd2f40bc443efc2693018cd86eebe8456
Pawel Psztyc <jarrodek@gmail.com>
2020-12-14 15:27:31 -0800

    chore: removing new exports from the main index file
    Signed-off-by: Pawel Psztyc <jarrodek@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,2 @@
 export {DemoPage} from './src/DemoPage';
 export {ApiDemoPage} from './src/ApiDemoPage';
-export * as EncodingHelpers from './src/EncodingHelpers';
-export { ExportHandlerMixin } from './src/ExportHandlerMixin';
--- index.js
+++ index.js
@@ -1,4 +1,2 @@
 export { DemoPage } from './src/DemoPage.js';
 export { ApiDemoPage } from './src/ApiDemoPage.js';
-export * as EncodingHelpers from './src/EncodingHelpers.js';
-export { ExportHandlerMixin } from './src/ExportHandlerMixin.js';
